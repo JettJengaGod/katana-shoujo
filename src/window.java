f76cbdaa7dc8c44d53376693435fe4a060764aa4
VicSto <vstolle@uci.edu>
2014-08-22 16:24:01 -0700

    Update window.java
    FUCKING GIMME IT

--- src/window.java
+++ src/window.java
@@ -1,4 +1,6 @@
 import javax.swing.JFrame;
+import java.awt.Dimension;
+import java.awt.Rectangle;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -9,13 +11,43 @@ import java.awt.event.MouseListener;
 public class window extends JFrame implements KeyListener, MouseListener
 {
 	//Boolean RUN determines if game is running
+	private int tits;
 	private boolean RUN;
 	//Determines the screen size
 	private static int SCREENWIDTH = 600;
 	private static int SCREENHEIGHT = 600;
+	//Screen
+	private Rectangle Background;
 	
 	public window(){
 		RUN = true;
-		
+		Background = new Rectangle(0, SCREENHEIGHT, SCREENWIDTH, SCREENHEIGHT);
 	}
+		
+	
+	private void createFrame()
+	{
+		//Add the level here.
+        setTitle("Katana Shoujo");
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        getContentPane().setPreferredSize(new Dimension(SCREENWIDTH - 10, SCREENHEIGHT - 10));
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+        setResizable(false);
+        setFocusable(true);
+        addNotify();
+        requestFocus();
+        addKeyListener(this);
+	}
+	
 }
+
+
+
+
+
+
+
+
+
