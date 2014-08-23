40396ea4893d19bc59cb57efedb59e70cdd6c019
Victor Stolle <vstolle@uci.edu>
2014-08-22 20:58:17 -0700

    This file contains the window for the game

---
+++ src/TheRealSlimShady.java
@@ -0,0 +1,35 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+ 
+/* FrameDemo.java requires no other files. */
+public class TheRealSlimShady {
+	//The size of the window
+	public static int SCREENWIDTH = 1280;
+	public static int SCREENHEIGHT = 720;
+
+	
+    private static void createFrame() {
+    	
+        //Create and set up the window.
+        JFrame frame = new JFrame("Katana Shoujo");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        JLabel emptyLabel = new JLabel("");
+        emptyLabel.setPreferredSize(new Dimension(SCREENWIDTH, SCREENHEIGHT));
+        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+ 
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
+    }
+ 
+    public static void main(String[] args) {
+
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createFrame();
+            }
+        });
+    }
+}
