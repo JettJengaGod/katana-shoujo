527735c22c9be7a78f27ab9e41ef957629cd872a
jet <jetlewis@ucdavis.edu>
2014-08-22 21:47:41 -0700

    Pushing shit

--- src/shit.java
+++ src/shit.java
@@ -2,9 +2,25 @@ import java.io.IOException;
 
 
 public class shit {
+	static parser p;
 	public static void main(String [] args) throws IOException
 	{
-		parser p = new parser("shit.txt");
-		p.printShit();
+		p = new parser("shit.txt");
+	}
+	public static void progress()
+	{
+		String working = p.Story.get(0);
+		String tag;
+		String name;
+		String quote;
+		tag = working.substring(0,working.indexOf(']'));
+		name = working.substring(working.indexOf(']')+1,working.indexOf(':'));
+		quote = working.substring(working.indexOf(':')+1);
+		//display(name: quote);
+	}
+	public static String next(String tag)
+	{
+		
+		return null;
 	}
 }
