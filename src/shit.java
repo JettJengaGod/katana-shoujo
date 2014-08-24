de9daae87869144972879b45561f2f61dedca50d
jet <jetlewis@ucdavis.edu>
2014-08-23 22:53:15 -0700

    drawing backgrounds and characters added along with some commenting

--- src/shit.java
+++ src/shit.java
@@ -8,92 +8,141 @@ import java.awt.event.*;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
-
+/*This is the shitty file that I chose to do fucking everything in cause I'm too lazy to seprate it out and I wanted to use shitty globals.
+ * Enjoy.
+ */
 public class shit extends JApplet implements KeyListener, MouseListener
 {
-    int state; //
-	public  int SCREENWIDTH = 1280;
-	public  int SCREENHEIGHT = 720;
-	int index = 0;
-	ArrayList<String> options = new ArrayList<String>();
-	String working;
-	parser p;
-	Image textBox;
-	int points;
-	String tag;
-	String quoteC = "", nameC = "Tornado Max";
+	//Globals
+    int state; //What type of thing we are at in the game. 1 is story panel 2 is options 3 is end
+	public  int SCREENWIDTH = 1280;// Screen width
+	public  int SCREENHEIGHT = 720;// Screen height
+	int index = 0; //Where we are in the story
+	ArrayList<String> options = new ArrayList<String>(); //An array list of the current options. 
+	String working;//The text of the current line from the text. Still needs to be parsed into tag name and quote.
+	parser p; //The object that holds the story in an array list p.Story
+	Image textBox;//Image that holds the text and the name.
+	Image obox;
+	Image bg;
+	Image c,c1,c2;
+	int points; //How many points your character is at.
+	String tag; //The tag that shows what the current line is
+	String quoteC = "", nameC = ""; //The lines we are currently displaying on the screen for the name and quote 
 	public void init()
 	{
 		options.add("");
 		options.add("");
-		options.add("");
-		state=1;
+		options.add("");//These 3 lines are to populate options.
+		state=1;//The first line will always be a story panel so the state is 1.
 		try {
-			textBox =ImageIO.read(new File("Text Boxes.png"));
-			p = new parser("TestRoute.txt");
-			progress();
+			obox = ImageIO.read(new File("options.png"));
+			textBox =ImageIO.read(new File("Text Boxes.png"));//reads in the text boxes file.
+			p = new parser("TestRoute.txt");//reads in the story file.
+			progress();//the function that calls going through to the next line 
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		resize(SCREENWIDTH,SCREENHEIGHT); //Makes the applet the size we want it
+		addMouseListener(this); //Lets us use mouse
+		addKeyListener(this); //Lets us use keyboard
+	}
+	public void paint(Graphics g)//function that puts everything on the screen
+	{
+		Graphics2D scene = (Graphics2D)g;//Not really sure what this is. Blame Victor
+		//use scene to draw the background
+		
+		Font q = new Font("Arial",Font.PLAIN,40);//Makes the font we use for options and story text
+		g.clearRect(0, 0, SCREENWIDTH, SCREENHEIGHT);//Clears the screen
+		g.setFont(q); //Sets the default font
+		if(state == 1)//Story text
+		{
+			drawTag(g);
+			g.drawImage(textBox, 0,0,null); //draw the story boxes
+			Font n = new Font("Arial",Font.PLAIN,48);//Makes the font for the names
+			g.setFont(n); //Sets the name font
+			scene.drawString(nameC,53,430); //Draws the current name
+			g.setFont(q);//sets the quote font 
+			if(!quoteC.contains("&"))
+			{
+				scene.drawString(quoteC, 40, 520);//Draws the current qote font
+			}
+			else
+			{
+				scene.drawString(quoteC.substring(0,quoteC.indexOf('&')), 40, 520);
+				quoteC = quoteC.substring(quoteC.indexOf('&')+1);
+				int i = 1;
+				while(quoteC.contains("&"))
+				{
+					scene.drawString(quoteC.substring(0,quoteC.indexOf('&')), 40, 520+50*i);
+					quoteC = quoteC.substring(quoteC.indexOf('&')+1);
+					i++;
+				}
+				scene.drawString(quoteC, 40, 520+50*i);
+			}
+		}
+		else if(state == 2)//Draws the options
+		{
+			scene.drawImage(obox, 0, 0, null);
+			scene.drawString(options.get(0), 220, 180);
+			scene.drawString(options.get(1), 220, 358);
+			scene.drawString(options.get(2), 220, 527);
+		}
+		else if(state == 3)//Game is over
+		{
+
+			scene.drawString("The end", 40, 520); //End screen
+		}
+		
+	}
+	public void drawTag(Graphics g)
+	{
+		try {
+			String b = tag.substring(tag.indexOf("B"), tag.indexOf("B")+1);
+			bg = ImageIO.read(new File(b+".png"));
+			g.drawImage(bg,0,0,null);
+			if(tag.contains("T"))
+			{
+				int ch1x = 200,ch1y = 200,ch2x = 600,ch2y = 200;
+				tag = tag.substring(tag.indexOf("T"));
+				String ch1 = tag.substring(0, 2);
+				c1 = ImageIO.read(new File(ch1+".png"));
+				String ch2 = tag.substring(2, 5);
+				c2 = ImageIO.read(new File(ch2+".png"));
+				g.drawImage(c1,ch1x,ch1y,null);
+				g.drawImage(c2,ch2x,ch2y,null);
+			}
+			else
+			{
+				int cx = 400, cy = 200;
+				String ch = tag.substring(tag.indexOf('C'));
+				c = ImageIO.read(new File(ch+".png"));
+				g.drawImage(c,cx,cy,null);
+			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		resize(SCREENWIDTH,SCREENHEIGHT);
-		addMouseListener(this);
-		addKeyListener(this);
 	}
-	public void paint(Graphics g)
+	public  void progress() //Function that goes to the next step in the story
 	{
-		Graphics2D scene = (Graphics2D)g;
-		//use scene to draw the background
-		
-		Font q = new Font("Arial",Font.PLAIN,40);
-		g.clearRect(0, 0, SCREENWIDTH, SCREENHEIGHT);
-		g.setFont(q);
-		if(state == 1)
+		String name; //Name of the char 
+		String quote;//Current quote
+		if(index<p.Story.size()-1) //Makes sure no index out of bounds errors happen (they shouldn't anyway)
 		{
-
-			g.drawImage(textBox, 0,0,null);
-			Font n = new Font("Arial",Font.PLAIN,48);
-			g.setFont(n);
-			scene.drawString(nameC,53,430);
-			g.setFont(q);
-			scene.drawString(quoteC, 40, 520);
-		}
-		else if(state == 2)
-		{
-			scene.drawString(options.get(0), 50, 50);
-			scene.drawString(options.get(1), 50, 250);
-			scene.drawString(options.get(2), 50, 450);
-		}
-		else
-		{
-
-			scene.drawString("The end", 40, 520);
-		}
-		
-	}
-	public  void run()
-	{
-		
-	}
-	public  void progress() 
-	{
-		String name;
-		String quote;
-		if(index<p.Story.size()-1){
-			working = p.Story.get(index);
-			if(working.equals("ending"))
+			working = p.Story.get(index); //Sets up the string we are working with
+			if(working.equals("ending"))//checks if we are at the end
 			{
-				state = 3;
-				System.out.println("End");
-				return;
+				state = 3; //Sets end state
+				System.out.println("End");//For the text part of game
+				return; //Exits
 			}
-			tag = working.substring(0,working.indexOf(']'));
-			if(working.contains(":"))
+			tag = working.substring(0,working.indexOf(']')); //Makes the tag
+			if(working.contains(":")) //If there is a person talking
 			{
-				name = working.substring(working.indexOf(']')+1,working.indexOf(':'));
-				quote = working.substring(working.indexOf(':')+1);
-				display(name,quote);
-				index = next(tag,index);
+				name = working.substring(working.indexOf(']')+1,working.indexOf(':')); //Set the person's name
+				quote = working.substring(working.indexOf(':')+1); //Set the quote
+				display(name,quote); //Shows the quote on the screen
+				index = next(tag,index); //Sets index  
 			}
 			else if(working.contains(">"))
 			{
@@ -174,8 +223,8 @@ public class shit extends JApplet implements KeyListener, MouseListener
 
 	public void choose(int x, int y)
 	{
-		int bW = 500, bH = 100;
-		int ox = 200, o1y = 150, o2y = 250, o3y = 350;
+		int bW = 874, bH = 81;
+		int ox = 216, o1y = 128, o2y = 297, o3y = 466;
 		if(ox < x && x < ox+bW)
 		{
 			if(o1y < y && y < o1y + bH)
@@ -183,12 +232,12 @@ public class shit extends JApplet implements KeyListener, MouseListener
 				index = next(tag,index,1);
 				progress();
 			}
-			if(o1y < y && y < o1y + bH)
+			if(o2y < y && y < o2y + bH)
 			{
 				index = next(tag,index,2);
 				progress();
 			}
-			if(o2y < y && y < o2y + bH)
+			if(o3y < y && y < o3y + bH)
 			{
 				index = next(tag,index,3);
 				progress();
